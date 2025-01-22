92c1ef11fb882aaca0f89542493c1069bc26170d
Marcos Bueno <marcos_vbp@outlook.com>
2025-01-22 03:25:53 -0300

    feat: add mongoDB connection

--- Projeto_Oficina/src/backend/index.ts
+++ Projeto_Oficina/src/backend/index.ts
@@ -3,6 +3,9 @@ import { server } from "./config/server"
 import dotenv from "dotenv"
 dotenv.config()
 
+import { connectDB } from "./config/connectDB"
+connectDB()
+
 const port = process.env.PORT || 3001
 
 server.listen(port, () => {
