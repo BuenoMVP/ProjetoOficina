873835c5e9bef9f2cec10342d453b1e17c17084e
Marcos Bueno <marcos_vbp@outlook.com>
2025-01-22 18:09:41 -0300

    feat: add Encontros table and CRUD

---
+++ Projeto_Oficina/src/backend/controllers/encontrosController.ts
@@ -0,0 +1,66 @@
+import { Request, Response } from "express";
+import schemaEncontros from "../models/Encontros";
+import { encontrosProps } from "../types/bdTypes";
+
+const encontrosController = {
+    postEncontro: async (req: Request, res: Response) => {
+        try {
+            const encontro: encontrosProps = { ...req.body };
+
+            const objEncontro = await schemaEncontros.create(encontro);
+
+            if (!objEncontro)
+                return res.status(400).json({ msg: "Encontro não criado!" });
+
+            res.status(201).json({ objEncontro, msg: "Encontro criado!" });
+        } catch (error) {
+            res.status(400).json({ "Erro ao adicionar encontro": error });
+        }
+    },
+
+    getAllEncontros: async (_req: Request, res: Response) => {
+        try {
+            const objEncontros = await schemaEncontros.find();
+
+            if (!objEncontros)
+                return res.status(404).json({ msg: "Encontros não encontrados!" });
+
+            res.status(200).send(objEncontros);
+        } catch (error) {
+            res.status(400).json({ "Erro ao resgatar encontros": error });
+        }
+    },
+
+    updateEncontro: async (req: Request, res: Response) => {
+        try {
+            const { id } = req.params;
+            const encontro: encontrosProps = { ...req.body };
+
+            const objEncontro = await schemaEncontros.findByIdAndUpdate(id, encontro);
+
+            if (!objEncontro)
+                return res.status(404).json({ msg: "Encontro não encontrado!" });
+
+            res.status(200).json({ objEncontro, msg: "Encontro atualizado!" });
+        } catch (error) {
+            res.status(400).json({ "Erro ao atualizar encontro": error });
+        }
+    },
+
+    deleteEncontro: async (req: Request, res: Response) => {
+        try {
+            const { id } = req.params;
+
+            const objEncontro = await schemaEncontros.findByIdAndDelete(id);
+
+            if (!objEncontro)
+                return res.status(404).json({ msg: "Encontro não encontrado!" });
+
+            res.status(200).json({ objEncontro, msg: "Encontro deletado!" });
+        } catch (error) {
+            res.status(400).json({ "Erro ao deletar encontro": error });
+        }
+    }
+}
+
+export default encontrosController
--- Projeto_Oficina/src/backend/routes/router.ts
+++ Projeto_Oficina/src/backend/routes/router.ts
@@ -2,12 +2,14 @@ import express, { Request, Response } from "express"
 import integrantesRouter from "./integrantes"
 import usuariosRouter from "./usuarios"
 import informacoesRouter from "./informacoes"
+import encontrosRouter from "./encontros"
 
 const router = express.Router()
 
 router.use('/integrantes', integrantesRouter)
 router.use('/usuarios', usuariosRouter)
 router.use('/informacoes', informacoesRouter)
+router.use('/encontros', encontrosRouter)
 
 router.use('/', (_req: Request, res: Response) => {
     res.send('Página inicial da api')
